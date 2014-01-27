522bfda92cd9f8fc49cc8206e0e5d802fb32e221
cmaghintay <cmaghintay@gmail.com>
2014-01-27 01:12:02 -0500

    Initial
    Rough Layout
    How new questions are switched in
    
    To Do:
    Validate Answers
    Keep Score
    Add real questions

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    function test() {
+        var question = getElementById("question");
+        console.log(question);
+        console.log(Q1);
+        console.log("Test Success!");
+        return false;
+    };
+    
+    var Q1 = ["What is the second letter of the alphabet?", "A", "B", "C"];
+    
+});
